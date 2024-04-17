d2ecc6f0cbaeca84352c04050352037f4f13f839
it-Shoeb <sshoeb577@gmail.com>
2024-04-17 16:15:32 +0530

    resolve bugs and done with some updates change light & dark icons to google cdn to assets clear button not working resolve resolve bug that in light/dark mode

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const toggle = document.getElementById("toggle");
 
 const btns = document.querySelectorAll(".btn");
 
-let toggleStatus = false;
+let toggleStatus = true;
 
 const container = document.querySelector(".container");
 const calculator = document.querySelector(".calculator");
@@ -20,16 +20,25 @@ toggleContainer.addEventListener("click", () => {
       btn.style.color = "#202d44";
       btn.style.boxShadow = "0.5px 0.5px 2px #000";
     });
+
+    toggle.style.background = "white"
+    toggle.style.borderColor = "#202d44"
+    toggleContainer.style.background = "#202d44"
+    
   } else {
     toggle.style.left = 0 + "%";
     toggleStatus = true;
-
+    
     //dark mode
     // document.body.style.backgroundColor = 'aliceblue'
     container.style.backgroundColor = "#161f2f";
     btns.forEach((btn) => {
       btn.style.backgroundColor = "#202d44";
       btn.style.color = "white";
+
+      toggle.style.background = "#202d44"
+      toggle.style.borderColor = "white"
+      toggleContainer.style.background = "white"
     });
   }
 });
@@ -40,23 +49,23 @@ let store = "";
 
 btns.forEach((btn) => {
   btn.addEventListener("click", () => {
+    
     if (btn.textContent != "Equal" && btn.textContent != "Clear") {
       store = store + btn.textContent;
       console.log(store);
       addends.textContent = store;
-    }
+    } 
 
     sum.textContent = eval(store);
 
     if (btn.textContent == "Equal") {
-      console.log(eval(store));
+      // console.log(eval(store));
       sum.textContent = eval(store);
-      store = "";
     }
 
     if (btn.textContent == "Clear") {
-      sum.textContent = " ";
-      addends.textContent = " ";
+      sum.textContent = "";
+      addends.textContent = "";
       store = "";
     }
   });
