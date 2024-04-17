dd3be868ed26acfae0fa60855c9ce21b77b8370d
it-Shoeb <sshoeb577@gmail.com>
2024-04-17 14:58:50 +0530

    Adding light/ dark mode funationlity and also use eval function to evalutate string into mathematical expression

--- script.js
+++ script.js
@@ -1,55 +1,88 @@
-const toggleContainer = document.getElementById('toggle-container')
-const toggle = document.getElementById('toggle')
+const toggleContainer = document.getElementById("toggle-container");
+const toggle = document.getElementById("toggle");
 
-const btns = document.querySelectorAll('.btn')
+const btns = document.querySelectorAll(".btn");
 
-let toggleStatus = false
+let toggleStatus = false;
 
-const container = document.querySelector('.container');
-const calculator = document.querySelector('.calculator');
+const container = document.querySelector(".container");
+const calculator = document.querySelector(".calculator");
 
-toggleContainer.addEventListener('click', () => {
-    if(toggleStatus){
-        toggleStatus = false;
-        toggle.style.left = 50+'%'
-        //light mode
-        // document.body.style.backgroundColor = 'rgb(63, 96, 126);'
-        container.style.backgroundColor = 'white'
-        btns.forEach((btn) => {
-            btn.style.backgroundColor = 'white'
-            btn.style.color = '#202d44'
-            btn.style.boxShadow = '0.5px 0.5px 2px #000'
-        })
-        
-        
-    }else{
-        toggle.style.left = 0+'%'
-        toggleStatus = true;
-        
-        //dark mode
-        // document.body.style.backgroundColor = 'aliceblue'
-        container.style.backgroundColor = '#161f2f'
-        btns.forEach((btn) => {
-            btn.style.backgroundColor = '#202d44'
-            btn.style.color = 'white'
-        })  
+toggleContainer.addEventListener("click", () => {
+  if (toggleStatus) {
+    toggleStatus = false;
+    toggle.style.left = 50 + "%";
+    //light mode
+    // document.body.style.backgroundColor = 'rgb(63, 96, 126);'
+    container.style.backgroundColor = "white";
+    btns.forEach((btn) => {
+      btn.style.backgroundColor = "white";
+      btn.style.color = "#202d44";
+      btn.style.boxShadow = "0.5px 0.5px 2px #000";
+    });
+  } else {
+    toggle.style.left = 0 + "%";
+    toggleStatus = true;
 
+    //dark mode
+    // document.body.style.backgroundColor = 'aliceblue'
+    container.style.backgroundColor = "#161f2f";
+    btns.forEach((btn) => {
+      btn.style.backgroundColor = "#202d44";
+      btn.style.color = "white";
+    });
+  }
+});
+
+const sum = document.querySelector(".sum");
+const addends = document.querySelector(".addends");
+let store = "";
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    if (btn.textContent != "Equal" && btn.textContent != "Clear") {
+      store = store + btn.textContent;
+      console.log(store);
+      addends.textContent = store;
     }
-})
 
-const sum = document.querySelector('.sum')
+    sum.textContent = eval(store);
 
-btns.forEach(btn => {
-    btn.addEventListener('click', () => {
-        console.log(btn.textContent)
+    if (btn.textContent == "Equal") {
+      console.log(eval(store));
+      sum.textContent = eval(store);
+      store = "";
+    }
 
-        sum.textContent = parseInt(sum.textContent + btn.textContent)
-        if(btn.textContent=='Equal'){
-            console.log( parseInt(sum.textContent))
-        }else if(btn.textContent=='Clear'){
-            sum.textContent = ''
-        }else{
+    if (btn.textContent == "Clear") {
+      sum.textContent = " ";
+      addends.textContent = " ";
+      store = "";
+    }
+  });
+});
 
-        }
-    })
+window.addEventListener("keydown", (e) => {
+  // console.log(e);
+  btns.forEach((btn) => {
+    if (btn.textContent == e.key) {
+      store = store + btn.textContent;
+      // console.log("keypress", store);
+      addends.textContent = store;
+    }
+
+    if (e.key == "Enter") {
+      sum.textContent = eval(store);
+    }
+
+    if (e.key == "Delete" || e.key == "Backspace") {
+      store = "";
+      addends.textContent = "";
+      sum.textContent = "";
+    }
+
+    // else if(e.key == 'Enter'){
+
+    // }
+  });
 });
